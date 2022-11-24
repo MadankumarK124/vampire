e49c42dcb74235ea49a4d9e1115298372ce38ef5
MADAN KUMAR K <MADAN KUMAR K@LAPTOP-IQ9B34OU>
2022-11-24 19:48:50 +0530

    commit

---
+++ src/test/java/demon/stephen.java
@@ -0,0 +1,13 @@
+package demon;
+
+import org.testng.annotations.Test;
+
+public class stephen {
+	@Test
+	public void salvator()
+	{
+		System.out.println("helina");
+		System.out.println("ceroline");
+	}
+
+}
